1f69ab4689a7618cd7bb68f91404c1380e3ccfb2
Leighton Shank <shanklt@jmu.edu>
2014-10-06 17:23:46 -0400

    Course validation and basic test
    Added a validation on the course duration field to ensure the value can
    only be an integer.  There is also now a single unit test for the
    Course model, which ensures that the model validation is working (and
    also shows that the testing is wired up correctly)

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
   validates :title, :description, :duration, :instructor, presence: true
   validates :title, uniqueness: true
-  validates :duration, numericality: {greater_than: 0}
+  validates :duration, numericality: {only_integer: true, greater_than: 0}
 end
