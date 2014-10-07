03ed1b7ad48706ace2bd4c0acecc8e19526be6a5
Leighton Shank <shanklt@jmu.edu>
2014-10-07 12:28:40 -0400

    added tests for a course

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -9,4 +9,41 @@ class CourseTest < ActiveSupport::TestCase
     assert course.errors[:instructor].any?
     assert course.errors[:duration].any?
   end
+
+  test "course duration must be greater than zero" do
+    course = Course.new(title: "Test Course",
+                        description: "zzz",
+                        instructor: "Herbert Nenninger")
+    course.duration = -1
+    assert course.invalid?
+    assert_equal ["must be greater than 0"], course.errors[:duration]
+
+    course.duration = 0
+    assert course.invalid?
+    assert_equal ["must be greater than 0"], course.errors[:duration]
+
+    course.duration = 1
+    assert course.valid?
+  end
+
+  test "course duration must be an integer" do
+    course = Course.new(title: "Test Course",
+                        description: "zzz",
+                        instructor: "Herbert Nenninger")
+    course.duration = 0.01
+    assert course.invalid?
+    assert_equal ["must be an integer"], course.errors[:duration]
+
+    course.duration = 1
+    assert course.valid?
+  end
+
+  test "course title must be unique" do
+    course = Course.new(title: courses(:canvas101).title,
+                        description: "zzz",
+                        instructor: "Herbert Nenninger",
+                        duration: 60)
+    assert course.invalid?
+    assert_equal ["has already been taken"], course.errors[:title]
+  end
 end
