5eb20dc0f238b9b4be1a5172895aace87dca0094
Leighton Shank <shanklt@jmu.edu>
2014-10-08 16:56:35 -0400

    fixed controller tests for sections
    Controller tests are now passing for sections.  Needed to setup
    fixtures, and then there was some tweaking to do because of the nested
    resources.

--- test/controllers/sections_controller_test.rb
+++ test/controllers/sections_controller_test.rb
@@ -2,48 +2,54 @@ require 'test_helper'
 
 class SectionsControllerTest < ActionController::TestCase
   setup do
-    @section = sections(:one)
+    @section = sections(:carrier_rm37)
+    @update = {
+      location: 'CIT Room 7',
+      starts_at: '2014-10-11 12:00:00',
+      seats: 6,
+      course_id: @section.course_id
+    }
   end
 
   test "should get index" do
-    get :index
+    get :index, course_id: @section.course_id
     assert_response :success
     assert_not_nil assigns(:sections)
   end
 
   test "should get new" do
-    get :new
+    get :new, course_id: @section.course_id
     assert_response :success
   end
 
   test "should create section" do
     assert_difference('Section.count') do
-      post :create, section: { course_id: @section.course_id, location: @section.location, seats: @section.seats, starts_at: @section.starts_at }
+      post :create, course_id: @section.course_id, section: @update
     end
 
-    assert_redirected_to section_path(assigns(:section))
+    assert_redirected_to course_section_path(@section.course, assigns(:section))
   end
 
   test "should show section" do
-    get :show, id: @section
+    get :show, id: @section, course_id: @section.course_id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @section
+    get :edit, id: @section, course_id: @section.course_id
     assert_response :success
   end
 
   test "should update section" do
-    patch :update, id: @section, section: { course_id: @section.course_id, location: @section.location, seats: @section.seats, starts_at: @section.starts_at }
-    assert_redirected_to section_path(assigns(:section))
+    patch :update, id: @section, course_id: @section.course_id, section: @update
+    assert_redirected_to course_section_path(@section.course, assigns(:section))
   end
 
   test "should destroy section" do
     assert_difference('Section.count', -1) do
-      delete :destroy, id: @section
+      delete :destroy, id: @section, course_id: @section.course_id
     end
 
-    assert_redirected_to sections_path
+    assert_redirected_to course_sections_path(@section.course)
   end
 end
