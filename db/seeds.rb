024a5b59932860625893e99f83805ba3417cf042
Leighton Shank <shanklt@jmu.edu>
2014-10-08 17:14:48 -0400

    db seeds for sections
    Updated seeds.rb to include sections in the course seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,85 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Clear out existing data
+Section.delete_all
 Course.delete_all
+
 # Canvas 101: The Philosophy
 Course.create!(title: 'Canvas 101: The Philosophy',
   description: %{Canvas IS different! In this one hour session, you will
     develop a roadmap for designing your course in a way that benefits you AND
     your students.},
   instructor: 'Nicole Wilson',
-  duration: 60)
+  duration: 60,
+  sections: Section.create!([
+    {
+      location: 'Carrier Library Room 37',
+      starts_at: '2014-12-09 14:00:00',
+      seats: 25
+    },
+    {
+      location: 'Rose Library Room 5308',
+      starts_at: '2014-12-16 09:00:00',
+      seats: 25
+    },
+  ]))
 # Canvas 113: Creating and Managing Content
 Course.create!(title: 'Canvas 113: Creating and Managing Content',
   description: %{Do you have course content files that need to make their way
     into Canvas? In this session you will add and organize content in your
     course.},
   instructor: 'Dave Stoops',
-  duration: 60)
+  duration: 60,
+  sections: Section.create!([
+    {
+      location: 'Carrier Library Room 37',
+      starts_at: '2014-12-10 14:00:00',
+      seats: 25
+    },
+    {
+      location: 'Rose Library Room 5308',
+      starts_at: '2014-12-17 09:00:00',
+      seats: 25
+    },
+  ]))
 # Canvas 211: Communication
 Course.create!(title: 'Canvas 211: Communication',
   description: %{Explore the many avenues for interaction and communication:
     instructor-student, student-student, and student-content.},
   instructor: 'Elaine Roberts',
-  duration: 60)
+  duration: 60,
+  sections: Section.create!([
+    {
+      location: 'Carrier Library Room 37',
+      starts_at: '2014-12-11 14:00:00',
+      seats: 25
+    },
+    {
+      location: 'Rose Library Room 5308',
+      starts_at: '2014-12-18 09:00:00',
+      seats: 25
+    },
+  ]))
 # Canvas 234: All Things Grading: Assignments, Assessments, Rubrics, and Speedgrader!
 Course.create!(title: %{Canvas 234: All Things Grading: Assignments,
   Assessments, Rubrics, and Speedgrader!},
   description: %{Speedgrader? Speedgrader indeed! Come to this workshop and
     leave on the fast track to grading.},
   instructor: 'Christie Liu',
-  duration: 120)
+  duration: 120,
+  sections: Section.create!([
+    {
+      location: 'Carrier Library Room 37',
+      starts_at: '2014-12-12 14:00:00',
+      seats: 25
+    },
+    {
+      location: 'Rose Library Room 5308',
+      starts_at: '2014-12-19 09:00:00',
+      seats: 25
+    },
+  ]))
 # Canvas 235: Educating with Grading: Rubrics, Feedback, Group Work and Peer Review
 Course.create!(title: %{Canvas 235: Educating with Grading: Rubrics, Feedback,
   Group Work and Peer Review},
@@ -49,4 +100,16 @@ Course.create!(title: 'Canvas Content Worksession',
     Content workshop, meet one-on-one with a CIT representative to get some
     help adding content to your course.},
   instructor: 'CIT Faculty Development',
-  duration: 60)
+  duration: 60,
+  sections: Section.create!([
+    {
+      location: 'Carrier Library Room 37',
+      starts_at: '2014-12-10 15:15:00',
+      seats: 25
+    },
+    {
+      location: 'Rose Library Room 5308',
+      starts_at: '2014-12-17 10:15:00',
+      seats: 25
+    },
+  ]))
