35bf2848dee9221d89c8a4da78331ef78190db3e
Jon Short <shortjw@jmu.edu>
2014-10-08 19:29:23 -0400

    Added role to users model.  Also changed profile information to display user role (temporary for demonstration purposes).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :instructor, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
   # devise modules
   devise :ldap_authenticatable, :registerable,
          :rememberable, :trackable
