8f828cfcbed51539c39986ff6640e704e8a14f0c
Leighton Shank <shanklt@jmu.edu>
2014-10-13 13:20:44 -0400

    Fix sections form and remove 'field-with-errors'
    Removed some extra <br> tags in the sections form that got left there
    from the scaffolding.  Also fixed the
    config/initializers/action_view.rb file so that it actually removes the
    ‘field-with-errors’ divs that rails adds.

--- config/initializers/action_view.rb
+++ config/initializers/action_view.rb
@@ -1,4 +1,4 @@
 # remove the default 'field_with_errors' class on field errors
-Rails.application.config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
   html_tag.html_safe
 }
