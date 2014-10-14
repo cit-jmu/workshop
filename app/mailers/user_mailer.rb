6bc5cc290ce544e3e3860d1300f5ad881dbb7285
Leighton Shank <shanklt@jmu.edu>
2014-10-14 18:05:54 -0400

    :+1: got the calendar invites working!
    Calendar invitations should be working now (as well as cancels when you
    drop a course) under all versions of Outlook.  Apparently Outlook likes
    it when the ics calendar event data is in the body of the email, so we
    put it there in addition to being an attachment.
    
    Tested in Outlook 2013 on Windows 7 as well as in Outlook Web Access
    and they both worked as expected.  Outlook for Mac 2011 is working too
    (on version 14.1.0 at least)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,11 +8,15 @@ class UserMailer < ActionMailer::Base
     @course = enrollment.section.course
 
   	attachments['workshop.ics'] = {
-      content_type: 'text/calendar; method=REQUEST',
+      content_type: 'application/ics; name="workshop.ics"',
       content: ical_invite
     }
 
-  	mail(to: @user.email, subject: "CIT Workshop enrollment for #{@course.title}")
+  	mail(to: @user.email, subject: "CIT Workshop enrollment for #{@course.title}") do |format|
+      format.text
+      format.html
+      format.ics { render text: ical_invite, content_type: "text/calendar; method=REQUEST"}
+    end
   end
 
   def unenroll_email(enrollment)
@@ -22,16 +26,21 @@ class UserMailer < ActionMailer::Base
     @course = enrollment.section.course
 
   	attachments['workshop.ics'] = {
-      content_type: 'text/calendar; method=CANCEL',
+      content_type: 'application/ics; name="workshop.ics"',
       content: ical_cancel
     }
 
-  	mail(to: @user.email, subject: "CIT Workshop course drop for #{@course.title}")
+  	mail(to: @user.email, subject: "CIT Workshop course drop for #{@course.title}") do |format|
+      format.text
+      format.html
+      format.ics { render text: ical_cancel, content_type: "text/calendar; method=CANCEL"}
+    end
   end
 
   private
   	def ical_invite
   	  cal = Icalendar::Calendar.new
+      cal.prodid = "-//James Madison University//CIT Workshop//EN"
       cal.ip_method = "REQUEST"
   	  cal.event do |e|
         e.uid = @enrollment.ical_event_uid
@@ -40,36 +49,35 @@ class UserMailer < ActionMailer::Base
   		  e.summary = @course.title
   		  e.description = @course.summary
   		  e.location = @section.location
-  		  e.ip_class = "PUBLIC"
         e.status = "CONFIRMED"
   		  e.organizer = Icalendar::Values::CalAddress.new("mailto:citsupport@jmu.edu",
                         cn: "Center for Instructional Technology")
   		  e.attendee = Icalendar::Values::CalAddress.new("mailto:#{@user.email}",
                        cn: @user.full_name,
                        cutype: "INDIVIDUAL",
-                       partstat: "NEEDS-ACTION")
+                       partstat: "NEEDS-ACTION",
+                       role: "REQ-PARTICIPANT")
   		end
   	  cal.to_ical
   	end
 
     def ical_cancel
   	  cal = Icalendar::Calendar.new
+      cal.prodid = "-//James Madison University//CIT Workshop//EN"
       cal.ip_method = "CANCEL"
   	  cal.event do |e|
         e.uid = @enrollment.ical_event_uid
   		  e.dtstart = @section.starts_at.strftime("%Y%m%dT%H%M%S")
   		  e.dtend = @section.ends_at.strftime("%Y%m%dT%H%M%S")
-  		  #e.summary = @course.title
-  		  #e.description = @course.summary
   		  e.location = @section.location
-  		  e.ip_class = "PUBLIC"
         e.status = "CANCELLED"
   		  e.organizer = Icalendar::Values::CalAddress.new("mailto:citsupport@jmu.edu",
                         cn: "Center for Instructional Technology")
   		  e.attendee = Icalendar::Values::CalAddress.new("mailto:#{@user.email}",
                        cn: @user.full_name,
                        cutype: "INDIVIDUAL",
-                       partstat: "NEEDS-ACTION")
+                       partstat: "NEEDS-ACTION",
+                       role: "REQ-PARTICIPANT")
   		end
   	  cal.to_ical
   	end
