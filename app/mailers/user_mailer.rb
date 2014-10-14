fded2a7ea76e555d3e2de20046faf5172377f87f
Jon Short <shortjw@jmu.edu>
2014-10-14 10:53:28 -0400

    Fixes for ICS formatting, Outlook is happy now.  Also added email to test fixtures to resolve testing error.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,23 +5,25 @@ class UserMailer < ActionMailer::Base
   	@user = user
   	@course = section.course.title
   	@date_and_time = section.date_and_time
-  	attachments.inline['calendar.ics'] = {:content_type => 'text/calendar; method=REQEST', :content => ical(user, section) }
+  	attachments['workshop.ics'] = {:content_type => 'text/calendar; method=REQEST', :content => create_ical(user, section) }
   	mail(to: @user.email, subject: "Enrollment: #{@course}")
   		#, content_type: 'text/calendar; method=REQUEST name="calendar.ics"',
   	  #content_disposition: "inline; filename=calendar.ics", body: '',
   		#content: ical(user, section), encoding: '8bit')
   end
 
-	def ical(user, section)
-	  event = Icalendar::Event.new
-	  event.dtstart = section.starts_at.strftime("%Y%m%dT%H%M%S")
-	  event.dtend = section.ends_at.strftime("%Y%m%dT%H%M%S")
-	  event.summary = section.course.title
-	  event.description = section.course.description
-	  event.location = section.location
-	  event.ip_class = "PUBLIC"
-	  event.organizer = "citsupport@jmu.edu"
-	  event.attendee = user.email
-	  event.to_ical
+	def create_ical(user, section)
+	  cal = Icalendar::Calendar.new
+	  cal.event do |e|
+		  e.dtstart = section.starts_at.strftime("%Y%m%dT%H%M%S")
+		  e.dtend = section.ends_at.strftime("%Y%m%dT%H%M%S")
+		  e.summary = section.course.title
+		  e.description = section.course.description
+		  e.location = section.location
+		  e.ip_class = "PUBLIC"
+		  e.organizer = "citsupport@jmu.edu"
+		  e.attendee = user.email
+		end
+	  cal.to_ical
 	end
 end
