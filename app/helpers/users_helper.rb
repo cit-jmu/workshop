3a13c07656e08a88b6bacc8ad50e7c062d9fd0d0
Leighton Shank <shanklt@jmu.edu>
2014-10-29 11:58:19 -0400

    cleanup the users#_form view
    refactored and cleaned it up a bit, taking advantage of helpers.  added
    a UserHelper:error_class? method to add the bootstrap ’.has-error’
    class for fields with a validation error.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,17 @@
 module UsersHelper
-  def profile_field(form, helper, attribute)
-    if can?(:update, @user) && !@user.in_ldap?
-      form.send helper, attribute, :class => 'form-control'
-    else
-      form.send helper, attribute, :class => 'form-control',
-                                   :disabled => true
-    end
+  def profile_field(form, helper, attribute, options = {})
+    options = options.reverse_merge(:class => '')
+    # add the bootstrap 'form-control' class
+    options[:class] += ' form-control'
+    # disable input unless the current user can :update this user and this
+    # user is not managed through LDAP
+    options[:disabled] = true unless can?(:update, @user) && !@user.in_ldap?
+    # pass this along to the form
+    form.send helper, attribute, options
+  end
+
+  def error_class?(attribute)
+    'has-error' if @user.errors[attribute].any?
   end
 
   def profile_nav_link_to(name, path)
