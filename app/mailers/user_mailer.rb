1aecbb89239e8435617469862f8e1dfed4019b2e
Leighton Shank <shanklt@jmu.edu>
2014-11-05 17:10:32 -0500

    Add VALARM to iCal invite
    Set a VALARM reminder in the iCal invite event for 1 day before
    the workshop.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,12 +8,12 @@ class UserMailer < ActionMailer::Base
     @section = enrollment.section
     @course = enrollment.section.course
 
-  	attachments['workshop.ics'] = {
+    attachments['workshop.ics'] = {
       content_type: 'application/ics; name="workshop.ics"',
       content: ical_invite
     }
 
-  	mail(to: @user.email, subject: "CIT Workshop enrollment for #{@course.title}") do |format|
+    mail(to: @user.email, subject: "CIT Workshop enrollment for #{@course.title}") do |format|
       format.text
       format.html
       format.ics { render text: ical_invite, content_type: "text/calendar; method=REQUEST"}
@@ -27,12 +27,12 @@ class UserMailer < ActionMailer::Base
     @section = enrollment.section
     @course = enrollment.section.course
 
-  	attachments['workshop.ics'] = {
+    attachments['workshop.ics'] = {
       content_type: 'application/ics; name="workshop.ics"',
       content: ical_cancel
     }
 
-  	mail(to: @user.email, subject: "CIT Workshop course drop for #{@course.title}") do |format|
+    mail(to: @user.email, subject: "CIT Workshop course drop for #{@course.title}") do |format|
       format.text
       format.html
       format.ics { render text: ical_cancel, content_type: "text/calendar; method=CANCEL"}
@@ -40,47 +40,54 @@ class UserMailer < ActionMailer::Base
   end
 
   private
-  	def ical_invite
-  	  cal = Icalendar::Calendar.new
-      cal.prodid = "-//James Madison University//CIT Workshop//EN"
-      cal.ip_method = "REQUEST"
-  	  cal.event do |e|
-        e.uid = "#{@enrollment.ical_event_uid}-#{@part.id}@cit.jmu.edu"
-  		  e.dtstart = @part.starts_at.strftime("%Y%m%dT%H%M%S")
-  		  e.dtend = @part.ends_at.strftime("%Y%m%dT%H%M%S")
-  		  e.summary = @course.title
-  		  e.description = @course.summary
-  		  e.location = @part.location
-        e.status = "CONFIRMED"
-  		  e.organizer = Icalendar::Values::CalAddress.new("mailto:citsupport@jmu.edu",
-                        cn: "Center for Instructional Technology")
-  		  e.attendee = Icalendar::Values::CalAddress.new("mailto:#{@user.email}",
-                       cn: @user.full_name,
-                       cutype: "INDIVIDUAL",
-                       partstat: "NEEDS-ACTION",
-                       role: "REQ-PARTICIPANT")
-  		end
-  	  cal.to_ical
-  	end
+  def ical_invite
+    cal = Icalendar::Calendar.new
+    cal.prodid = "-//James Madison University//CIT Workshop//EN"
+    cal.ip_method = "REQUEST"
+    cal.event do |e|
+      e.uid = "#{@enrollment.ical_event_uid}-#{@part.id}@cit.jmu.edu"
+      e.dtstart = @part.starts_at.strftime("%Y%m%dT%H%M%S")
+      e.dtend = @part.ends_at.strftime("%Y%m%dT%H%M%S")
+      e.summary = @course.title
+      e.description = @course.summary
+      e.location = @part.location
+      e.status = "CONFIRMED"
+      e.organizer = Icalendar::Values::CalAddress.new("mailto:citsupport@jmu.edu",
+                      cn: "Center for Instructional Technology")
+      e.attendee = Icalendar::Values::CalAddress.new("mailto:#{@user.email}",
+                     cn: @user.full_name,
+                     cutype: "INDIVIDUAL",
+                     partstat: "NEEDS-ACTION",
+                     role: "REQ-PARTICIPANT")
 
-    def ical_cancel
-  	  cal = Icalendar::Calendar.new
-      cal.prodid = "-//James Madison University//CIT Workshop//EN"
-      cal.ip_method = "CANCEL"
-  	  cal.event do |e|
-        e.uid = "#{@enrollment.ical_event_uid}-#{@part.id}@cit.jmu.edu"
-  		  e.dtstart = @part.starts_at.strftime("%Y%m%dT%H%M%S")
-  		  e.dtend = @part.ends_at.strftime("%Y%m%dT%H%M%S")
-  		  e.location = @part.location
-        e.status = "CANCELLED"
-  		  e.organizer = Icalendar::Values::CalAddress.new("mailto:citsupport@jmu.edu",
-                        cn: "Center for Instructional Technology")
-  		  e.attendee = Icalendar::Values::CalAddress.new("mailto:#{@user.email}",
-                       cn: @user.full_name,
-                       cutype: "INDIVIDUAL",
-                       partstat: "NEEDS-ACTION",
-                       role: "REQ-PARTICIPANT")
-  		end
-  	  cal.to_ical
-  	end
+      # schedule an alarm for 1 day prior to workshop
+      e.alarm do |a|
+        a.action = "DISPLAY"
+        a.summary = "CIT Workshop reminder"
+        a.trigger = '-P1DT0H0M0S'
+      end
+    end
+    cal.to_ical
+  end
+
+  def ical_cancel
+    cal = Icalendar::Calendar.new
+    cal.prodid = "-//James Madison University//CIT Workshop//EN"
+    cal.ip_method = "CANCEL"
+    cal.event do |e|
+      e.uid = "#{@enrollment.ical_event_uid}-#{@part.id}@cit.jmu.edu"
+      e.dtstart = @part.starts_at.strftime("%Y%m%dT%H%M%S")
+      e.dtend = @part.ends_at.strftime("%Y%m%dT%H%M%S")
+      e.location = @part.location
+      e.status = "CANCELLED"
+      e.organizer = Icalendar::Values::CalAddress.new("mailto:citsupport@jmu.edu",
+                      cn: "Center for Instructional Technology")
+      e.attendee = Icalendar::Values::CalAddress.new("mailto:#{@user.email}",
+                     cn: @user.full_name,
+                     cutype: "INDIVIDUAL",
+                     partstat: "NEEDS-ACTION",
+                     role: "REQ-PARTICIPANT")
+    end
+    cal.to_ical
+  end
 end
