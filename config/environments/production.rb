a3a9a0f4cab395b15c80bdc60dbedce02981f88c
Leighton Shank <shanklt@jmu.edu>
2014-12-05 14:54:39 -0500

    Fix error when enrolling users
    The production.rb environment config file didn't have the ActionMailer
    mail server settings.  Now it does.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # ActionMailer settings
+  config.action_mailer.default_url_options = { :host => 'workshop.cit.jmu.edu' }
+  config.action_mailer.smtp_settings = {
+    address: ENV['mail_server'],
+    port: ENV['mail_port'],
+    domain: ENV['mail_domain']
+  }
 end
