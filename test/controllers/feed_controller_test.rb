5da2a5ce48d20fb49eea4018a998b479fb396e66
Leighton Shank <shanklt@jmu.edu>
2014-12-08 16:43:36 -0500

    add basic feed controller tests

--- test/controllers/feed_controller_test.rb
+++ test/controllers/feed_controller_test.rb
@@ -2,13 +2,17 @@ require 'test_helper'
 
 class FeedControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
-    assert_response :success
+    [:json, :atom, :rss, :csv].each do |format|
+      get :index, format: format
+      assert_response :success
+    end
   end
 
   test "should get upcoming" do
-    get :upcoming
-    assert_response :success
+    [:json, :atom, :rss, :csv].each do |format|
+      get :upcoming, format: format
+      assert_response :success
+    end
   end
 
 end
