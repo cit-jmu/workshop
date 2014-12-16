4270c229ba7cd4d68e4911659d0e8a0f3789ea0b
Leighton Shank <shanklt@jmu.edu>
2014-12-16 14:28:47 -0500

    remove redundant `after_sign_in_path_for` method
    The `after_sign_in_path_for` method in the ApplicationController was
    duplicating the default Devise functionality.  The only different piece
    was that we had mapped the users#show action to the named route
    'dashboard', whereas Devise will automatically look for a named route
    for a resource, '<resource>_root'.  This was the only place
    'dashboard_path' was referenced, so removed it and renamed the route to
    'user_root'.  So now upon successful authentication, Devise will
    redirect to 'user_root_path'.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-    dashboard_path
-  end
 end
