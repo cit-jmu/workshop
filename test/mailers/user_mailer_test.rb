907348638517d44fd0a0bd352c5893707002a09d
Leighton Shank <shanklt@jmu.edu>
2015-09-01 12:13:45 -0400

    wrapped up the mailer tests
    removed the fixtures, went with just asserting for specific content
    pieces in the tests themselves.

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -29,17 +29,19 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match /^multipart\/alternative/, email.parts[0].content_type
     assert_equal 3, email.parts[0].parts.length
 
-    assert_equal 'text/plain; charset=UTF-8',
-                 email.parts[0].parts[0].content_type
-    #assert_equal read_fixture('enroll_email.text').join,
-    #             email.parts[0].parts[0].body.to_s
+    body_parts = email.parts[0].parts
+    assert_equal 'text/plain; charset=UTF-8', body_parts[0].content_type
+    assert_equal 'text/html; charset=UTF-8', body_parts[1].content_type
+    assert_equal 'text/calendar; charset=UTF-8', body_parts[2].content_type
 
-    assert_equal 'text/html; charset=UTF-8',
-                 email.parts[0].parts[1].content_type
-    #assert_equal read_fixture('enroll_email.html').join,
-    #             email.parts[0].parts[1].body.to_s
-
-    assert_equal 'text/calendar; charset=UTF-8', email.parts[0].parts[2].content_type
+    # test the html and text parts for specific content
+    body_parts[0..1].each do |part|
+      body = part.body.to_s
+      assert body.include?('Curious'), 'body should include first name'
+      assert body.include?('CIT Room7'), 'body should include location'
+      assert body.include?(@part.date_and_time), 'body should include date and time'
+      assert body.include?('Canvas 101: The Philosophy'), 'body should include course title'
+    end
 
     # this is the non-inline attachment.  both inline and non-inline are
     # needed to work with Outlook + other email clients
@@ -48,14 +50,89 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "unenroll email" do
+    email = UserMailer.unenroll_email(@enrollment, @part).deliver_now
+    assert_not ActionMailer::Base.deliveries.empty?
 
+    assert_equal ['no-reply@jmu.edu'], email.from
+    assert_equal ['george@test.com'], email.to
+    assert_equal 'CIT Workshop course drop for Canvas 101: The Philosophy',
+                 email.subject
+
+    assert email.multipart?
+    assert_equal 2, email.parts.length
+
+    # the first part is the text, html, and inline ics attachemnt
+    assert_match /^multipart\/alternative/, email.parts[0].content_type
+    assert_equal 3, email.parts[0].parts.length
+
+    body_parts = email.parts[0].parts
+    assert_equal 'text/plain; charset=UTF-8', body_parts[0].content_type
+    assert_equal 'text/html; charset=UTF-8', body_parts[1].content_type
+    assert_equal 'text/calendar; charset=UTF-8', body_parts[2].content_type
+
+    # test the html and text parts for specific content
+    body_parts[0..1].each do |part|
+      body = part.body.to_s
+      assert body.include?('Curious'), 'body should include first name' 
+      assert body.include?('Canvas 101: The Philosophy'), 'body should include course title'
+    end
+
+    # this is the non-inline attachment.  both inline and non-inline are
+    # needed to work with Outlook + other email clients
+    assert_equal 'application/ics; charset=UTF-8; name=workshop.ics',
+      email.parts[1].content_type
   end
 
   test "reminder email" do
+    email = UserMailer.reminder_email(@enrollment).deliver_now
+    assert_not ActionMailer::Base.deliveries.empty?
 
+    assert_equal ['no-reply@jmu.edu'], email.from
+    assert_equal ['george@test.com'], email.to
+    assert_equal 'CIT Workshop reminder for Canvas 101: The Philosophy',
+                 email.subject
+
+    assert email.multipart?
+    assert_equal 2, email.parts.length
+
+    assert_equal 'text/plain; charset=UTF-8', email.parts[0].content_type
+    assert_equal 'text/html; charset=UTF-8', email.parts[1].content_type
+
+    # make sure that the correct info gets injected into each email body part
+    email.parts.each do |part|
+      body = part.body.to_s
+      # check body for first_name
+      assert body.include?('Curious'), 'body should include first name'
+      # and course.title
+      assert body.include?('Canvas 101: The Philosophy'), 'body should include course title'
+    end
   end
 
   test "alert email" do
+    email = UserMailer.alert_email(@enrollment).deliver_now
+    assert_not ActionMailer::Base.deliveries.empty?
+
+    assert_equal ['no-reply@jmu.edu'], email.from
+    assert_equal ['notify@test.com'], email.to
+    assert_equal 'CIT Enrollment Alert for Canvas 101: The Philosophy',
+                 email.subject
+
+    assert email.multipart?
+    assert_equal 2, email.parts.length
+
+    assert_equal 'text/plain; charset=UTF-8', email.parts[0].content_type
+    assert_equal 'text/html; charset=UTF-8', email.parts[1].content_type
+
+    # make sure that the correct info gets injected into each email body part
+    email.parts.each do |part|
+      body = part.body.to_s
+      # check body for first_name
+      assert body.include?('Curious'), 'body should include first name'
+      # and course.title
+      assert body.include?('Canvas 101: The Philosophy'), 'body should include course title'
+      # and the user's email
+      assert body.include?('george@test.com'), 'body should include user email'
+    end
 
   end
 
@@ -72,9 +149,17 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal 2, email.parts.length
 
     assert_equal 'text/plain; charset=UTF-8', email.parts[0].content_type
-    assert_equal read_fixture('evaluation_email.text').join, email.parts[0].body.to_s
-
     assert_equal 'text/html; charset=UTF-8', email.parts[1].content_type
-    assert_equal read_fixture('evaluation_email.html').join, email.parts[1].body.to_s
+
+    # make sure that the correct info gets injected into each email body part
+    email.parts.each do |part|
+      body = part.body.to_s
+      # check body for first_name
+      assert body.include?('Curious'), 'body should include first name'
+      # and course.title
+      assert body.include?('Canvas 101: The Philosophy'), 'body should include course title'
+      # and the evaluation_url
+      assert body.include?('http://jmu.co1.qualtrics.com/SE/?SID=SV_82MNygLpNathnG5&course=Canvas+101%3A+The+Philosophy&enrollment_id=428823752&instructor=Professor+Wiseman'), 'body should include enrollment url'
+    end
   end
 end
