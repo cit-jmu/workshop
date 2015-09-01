097bbfe1daa84ad60b4ec624780cd0f6f2ce5ed4
Leighton Shank <shanklt@jmu.edu>
2015-09-01 17:45:52 -0400

    Add Strict-Transport-Security header
    the HTTP Strict-Transport-Security header (HSTS) response header will
    tell the client that this domain is meant to load over HTTPS, and the
    client will continue to force requests over HTTPS (even if the original
    request is for HTTP) until the max-age setting is hit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Add a HSTS (HTTPS Strict Transport Security) header
+  before_filter :add_strict_transport_security_header
+
+  def add_strict_transport_security_header
+    max_age = 60 * 60 * 24 * 365 # one year
+
+    # use an X- header in development to show it's getting set, but not
+    # enabling it (since we're not running https in development)
+    header_name = Rails.env.development?  ? 'X-Strict-Transport-Security'
+                                          : 'Strict-Transport-Security'
+
+    response.header[header_name] = "max-age=#{max_age}; includeSubDomains";
+  end
 end
