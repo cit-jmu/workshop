fa748d9cdff1c3381864113a55046b895208603a
Jen Short <short2jw@jmu.edu>
2016-01-14 16:13:05 -0500

    Added a default local configuration in the event that the local_settings.yml file doesn't exist and updated the example file to use _ instead of -for consistency.

--- config/initializers/local_settings.rb
+++ config/initializers/local_settings.rb
@@ -1,4 +1,23 @@
 # Loads local_settings.yml for site-specific configuration values
 # Access these values via Rails.application.config.x["first-level"]["second-level"]
+local_defaults = {
+  identity: {
+    site_name: "My Workshops",
+    site_owner: "Workshop Site Owner",
+    replyto: "email@yourdomain.com",
+    site_owner_link: "http://mydomain.com",
+    site_owner_link_text: "My Home Page"
+  },
+  terminology: {
+    course: 'Course',
+    instructor: 'Instructor',
+    workshop: 'Workshop',
+    section: 'Section'
+  }
+}
 
-Rails.application.config.x = Rails.application.config_for(:local_settings);
+if File.exist?(File.join("config", "local_settings.yml"))
+  Rails.application.config.x = Rails.application.config_for(:local_settings);
+else
+  Rails.application.config.x = local_defaults
+end
