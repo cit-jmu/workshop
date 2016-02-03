8bd6ef97d7a22c5d09342b033684aceccd75fb4d
Jon Short <shortjw@jmu.edu>
2016-02-03 10:23:17 -0500

    merged local_settings.rb initializer from local-site-config branch

--- config/initializers/local_settings.rb
+++ config/initializers/local_settings.rb
@@ -1,18 +1,18 @@
 # Loads local_settings.yml for site-specific configuration values
 # Access these values via Rails.application.config.x["first-level"]["second-level"]
 local_defaults = {
-  identity: {
-    site_name: "My Workshops",
-    site_owner: "Workshop Site Owner",
-    replyto: "email@yourdomain.com",
-    site_owner_link: "http://mydomain.com",
-    site_owner_link_text: "My Home Page"
+  "identity" => {
+    "site_name" => "My Workshops",
+    "site_owner" => "Workshop Site Owner",
+    "replyto" => "email@yourdomain.com",
+    "site_owner_link" => "http://mydomain.com",
+    "site_owner_link_text" => "My Home Page"
   },
-  terminology: {
-    course: 'Course',
-    instructor: 'Instructor',
-    workshop: 'Workshop',
-    section: 'Section'
+  "terminology" => {
+    "course" => 'Course',
+    "instructor" => 'Instructor',
+    "workshop" => 'Workshop',
+    "section" => 'Section'
   }
 }
 
