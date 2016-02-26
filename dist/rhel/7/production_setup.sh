328f480e20a4723d442c6a7ccef9076bc3d37ff2
Jon Short <shortjw@jmu.edu>
2016-02-26 15:22:05 -0500

    Enable puma daemon

--- dist/rhel/7/production_setup.sh
+++ dist/rhel/7/production_setup.sh
@@ -4,4 +4,5 @@ cp -a $current/dist/rhel/7/backup-db /usr/local/bin/
 cp -a $current/dist/rhel/7/puma-init /usr/local/bin/
 cp -a $current/dist/rhel/7/puma.service /usr/lib/systemd/system/
 systemctl daemon-reload
+systemctl enable puma.service
 chmod a+x /usr/local/bin/backup-db /usr/local/bin/puma-init
