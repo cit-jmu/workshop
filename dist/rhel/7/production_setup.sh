b943a532ee67fb1f7a28073ecfb999b53fc5fd09
Jon Short <shortjw@jmu.edu>
2016-03-01 15:21:27 -0500

    Add send-reminders support script and crontab entries

--- dist/rhel/7/production_setup.sh
+++ dist/rhel/7/production_setup.sh
@@ -2,7 +2,10 @@
 current=/home/deploy/apps/workshop/current
 cp $current/dist/rhel/7/backup-db /usr/local/bin/
 cp $current/dist/rhel/7/puma-init /usr/local/bin/
+cp $current/dist/rhel/7/send-reminders /usr/local/bin/
 cp $current/dist/rhel/7/puma.service /usr/lib/systemd/system/
-chmod a+x /usr/local/bin/backup-db /usr/local/bin/puma-init
+chmod a+x /usr/local/bin/backup-db /usr/local/bin/puma-init /usr/local/bin/send-reminders
+crontab -u deploy -l ; echo "0 9 * * * /usr/local/bin/workshop-send-reminders >> /home/deploy/apps/workshop/shared/log/cron.log 2>&1" | sort - | uniq - | crontab -u deploy -
+crontab -u postgres -l ; echo "0 0 * * * /usr/local/bin/backup-db workshop >> /var/log/backup-db.log 2>&1" | sort - | uniq - | crontab -u postgres -
 systemctl daemon-reload
 systemctl enable puma.service
